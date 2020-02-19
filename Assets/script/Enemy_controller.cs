8318ca4509ecd6444fb1043aeed9f6d4b5fb57e7
daqjjang <daqjjang@gmail.com>
2020-02-19 21:17:31 +0900

    test

---
+++ Assets/script/Enemy_controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_controller : MonoBehaviour {
+    public float lookRadius = 10.0f;
+    Transform target;
+        NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+//        Quaternion lookRotation= Quaternion.LookRotation(new Vector3(di))
+    }
+}
---
+++ Assets/script/attack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour {
+    public bool enemy_hit=false;
+    public Vector3 hit_effect_pos;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log(other.name);
+        enemy_hit = true;
+        hit_effect_pos= other.transform.position;
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        enemy_hit = false;
+    }
+}
